2a84e56f57786bf09f0eaaf29391f4a62a274db3
Nazarenko Serhii <serega84.kh@gmail.com>
2022-09-04 23:10:44 +0300

    homework3

---
+++ Java_Base_28.08/src/main/java/com/hillel/nazarenko/lessons/lessons3/homework/Main.java
@@ -0,0 +1,14 @@
+package com.hillel.nazarenko.lessons.lessons3.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10, b = 8, c = 5;
+        int volume = a * b * c;
+        int length = 4 * (a + b + c);
+        System.out.println("Объём параллелепипеда " + volume);
+        System.out.println("Сумма длин всех сторон параллелепипеда " + length);
+
+
+    }
+
+}
