f09dbb42eb567701e94faa0977e87f505509e44f
Nazarenko Serhii <serega84.kh@gmail.com>
2022-09-09 16:40:57 +0300

    homework4

---
+++ Java_Base_28.08/src/main/java/com/hillel/nazarenko/lessons4/homework4/Main.java
@@ -0,0 +1,15 @@
+package com.hillel.nazarenko.lessons4.homework4;
+
+public class Main {
+    public static void main(String[] args) {
+        int lyWar = 13, lyArch = 24, lyHors = 46;
+        int mynWar = 9, mynArch = 35, mynHors = 12;
+        int war = 860;
+        final double coef = 1.5;
+        System.out.println(" Attack Power Ly : " + (lyWar + lyArch + lyHors) * war);
+        System.out.println(" Attack Power Myn : " + (mynWar + mynArch + mynHors) * (int) (war * coef));
+
+    }
+
+
+}
