6a7e9cd46a274295c44f33e7ff8a8eade04f3f87
Nazarenko Serhii <serega84.kh@gmail.com>
2022-09-21 22:34:25 +0300

    homework7.update

--- Java_Base_28.08/src/main/java/com/hillel/nazarenko/lessons/lessons7/homework7/Main.java
+++ Java_Base_28.08/src/main/java/com/hillel/nazarenko/lessons/lessons7/homework7/Main.java
@@ -1,39 +1,30 @@
 package com.hillel.nazarenko.lessons.lessons7.homework7;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] team1 = new int[25];
-        int[] team2 = new int[25];
-        for (int a = 0; a < team1.length; a++) {
+        int length = 25;
+        int[] team1 = new int[length];
+        int[] team2 = new int[length];
+
+        for (int a = 0; a < length; a++) {
             team1[a] = (int) ((Math.random() * 41));
-            if (team1[a] < 18) {
+            team2[a] = (int) ((Math.random() * 41));
+            if (team1[a] < 18 || team2[a] < 18) {
                 a--;
-            } else {
-                System.out.print(team1[a] + "  ");
             }
         }
-        System.out.println();
-        for (int b = 0; b < team2.length; b++) {
-            team2[b] = (int) ((Math.random() * 41));
-            if (team2[b] < 18) {
-                b--;
-            } else {
-                System.out.print(team2[b] + "  ");
-            }
-        }
-        System.out.println();
+        System.out.println(Arrays.toString(team1));
+        System.out.println(Arrays.toString(team2));
         int team1Age = 0;
         int team2Age = 0;
-        for (int a = 0; a < team1.length; a++) {
+        for (int a = 0; a < length; a++) {
             team1Age += team1[a];
+            team2Age += team2[a];
         }
-        System.out.println("Средний возраст команды 1 : " + team1Age / team1.length);
-
-        for (int b = 0; b < team1.length; b++) {
-            team2Age += team2[b];
-        }
-        System.out.println("Средний возраст команды 2 : " + team2Age / team2.length);
-
-
+        System.out.println("Средний возраст команды 1 : " + (double) team1Age / length);
+        System.out.println("Средний возраст команды 2 : " + (double) team2Age / length);
     }
 }
