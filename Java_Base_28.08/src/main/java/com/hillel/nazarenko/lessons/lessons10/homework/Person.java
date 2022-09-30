25882b3a164273b86663bfece2025a9fb7727d63
Nazarenko Serhii <serega84.kh@gmail.com>
2022-09-30 12:54:53 +0300

    homework10

--- Java_Base_28.08/src/main/java/com/hillel/nazarenko/lessons/lessons10/homework/Person.java
+++ Java_Base_28.08/src/main/java/com/hillel/nazarenko/lessons/lessons10/homework/Person.java
@@ -3,22 +3,23 @@ package com.hillel.nazarenko.lessons.lessons10.homework;
 public class Person {
     public static void main(String[] args) {
 
-        String w="Will",s="Smith",n="New York",p="+12125555555";
-        String persona1 =personInfo(w,s,n,p);
+        String w = "Will", s = "Smith", n = "New York", p = "+12125555555";
+        String persona1 = personInfo(w, s, n, p);
         System.out.println(persona1);
 
-        String j="Jackie",c="Chan",sh="Shanghai",ph="+214444444";
-        String persona2 =personInfo(j,c,sh,ph);
+        String j = "Jackie", c = "Chan", sh = "Shanghai", ph = "+214444444";
+        String persona2 = personInfo(j, c, sh, ph);
         System.out.println(persona2);
 
-        String sher="Sherlock",h="Holmes",l="London",pn="+020777777";
-        String persona3 =personInfo(sher,h,l,pn);
+        String sher = "Sherlock", h = "Holmes", l = "London", pn = "+020777777";
+        String persona3 = personInfo(sher, h, l, pn);
         System.out.println(persona3);
 
 
     }
-    static String personInfo(String n,String s,String c,String ph){
-        String persona ="Позвонить гражданину " + n +" "+ s + " из города " + c + " можно по номеру " + ph;
+
+    static String personInfo(String n, String s, String c, String ph) {
+        String persona = "Позвонить гражданину " + n + " " + s + " из города " + c + " можно по номеру " + ph;
         return persona;
     }
 }
